69423ce79be38f828f1bf2d3ea94267072da9c9d
MRPE1970 <marcelo@informa.solutions>
2025-07-30 15:12:15 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import CatalogPage from './pages/CatalogPage';
 import AdminPage from './pages/AdminPage';
 import CheckoutPage from './pages/CheckoutPage';
-import ProductDetailsPage from './pages/ProductDetailsPage';
-import SoftwareFormPage from './pages/SoftwareFormPage';
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/catalog" element={<CatalogPage />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/product" element={<ProductDetailsPage />} />
-        <Route path="/admin/new" element={<SoftwareFormPage />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
